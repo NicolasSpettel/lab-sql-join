bf3586aa7993e192e80899d2ac8172d0b59de854
Nicolas Spettel <nicolas.spettel1991@gmail.com>
2022-09-16 17:49:16 +0200

    Lab SQL Joins format

--- Lab SQL Joins.sql
+++ Lab SQL Joins.sql
@@ -1,44 +1,44 @@
-use sakila;
+USE sakila;
 
 -- List the number of films per category.
-select * from film_category;
-select * from category;
-select count(film_id) as number_of_films, name as category from film_category f
-join category c 
-using(category_id)
-group by category_id;
+SELECT * FROM film_category;
+SELECT * FROM category;
+SELECT COUNT(film_id) AS number_of_films, name AS category FROM film_category f
+JOIN category c 
+USING(category_id)
+GROUP BY category_id;
 
 -- Display the first and the last names, as well as the address, of each staff member.
-select * from staff;
-select * from address;
-select first_name, last_name, address from staff s
-join address a 
-on s.address_id = a.address_id;
+SELECT * FROM staff;
+SELECT * FROM address;
+SELECT first_name, last_name, address FROM staff s
+JOIN address a 
+ON s.address_id = a.address_id;
 
 -- Display the total amount rung up by each staff member in August 2005.
-select * from staff;
-select * from payment;
+SELECT * FROM staff;
+SELECT * FROM payment;
 
-select staff_id, first_name, last_name, sum(amount) as rung_up from payment p
-right outer join staff s 
-using(staff_id)
-where date_format(payment_date, '%M') = 'August'
-and date_format(payment_date, '%Y') = '2005'
-group by staff_id;
+SELECT staff_id, first_name, last_name, SUM(amount) AS rung_up FROM payment p
+RIGHT OUTER JOIN staff s 
+USING(staff_id)
+WHERE DATE_FORMAT(payment_date, '%M') = 'August'
+AND DATE_FORMAT(payment_date, '%Y') = '2005'
+GROUP BY staff_id;
 
 -- List all films and the number of actors who are listed for each film.
-select * from film;
-select * from film_actor;
-select title, count(actor_id) as number_of_actors from film f
-left join film_actor a
-using(film_id)
-group by title;
+SELECT * FROM film;
+SELECT * FROM film_actor;
+SELECT title, COUNT(actor_id) AS number_of_actors FROM film f
+LEFT JOIN film_actor a
+USING(film_id)
+GROUP BY title;
 
 -- Using the payment and the customer tables as well as the JOIN command, list the total amount paid by each customer. List the customers alphabetically by their last names.
-select * from payment;
-select * from customer;
-select first_name, last_name, sum(amount) as total_amount_paid from customer c
-join payment p
-using(customer_id)
-group by customer_id
-order by last_name;
+SELECT * FROM payment;
+SELECT * FROM customer;
+SELECT first_name, last_name, SUM(amount) AS total_amount_paid FROM customer c
+JOIN payment p
+USING(customer_id)
+GROUP BY customer_id
+ORDER BY last_name;
